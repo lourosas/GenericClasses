ded0d83f07d163f7876145f6a99ebc88376bab77
lourosas <lourosas@gmail.com>
2018-01-08 11:41:52 -0700

    Add files via upload

---
+++ GenericViewMain.java
@@ -0,0 +1,13 @@
+/*
+*/
+import java.lang.*;
+import java.util.*;
+import myclasses.*;
+import rosas.lou.*;
+import rosas.lou.generic.GenericView;
+
+public class GenericViewMain{
+   public static void main(String [] args){
+      GenericView gv = new GenericView("Generic Window");
+   }
+}
